cf16d8bf145c80a2859d6da43fccb2c69033fded
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-08-04 22:18:50 +0200

    Add event listener for menu buttons and log click events

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './styles.css'
 
-console.log("hello")
+import menu from './data/menu.json'
+
+const menuButtons = document.querySelectorAll(".options")
+
+menuButtons.forEach(button => {
+  button.addEventListener("click", (event) => handleMenuClick(event.target.value))
+})
+
+function handleMenuClick(value) {
+  console.log("menu button:", value)
+}
+
+console.log(menu)
