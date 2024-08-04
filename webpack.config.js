447a00fe648a242c60516adb5bfd7ea58d1e7fb5
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-08-04 20:58:37 +0200

    Update webpack configuration for development mode and add Babel loader for JavaScript files

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,33 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: "./src/index.js",
   devtool: "inline-source-map",
   module: {
     rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      },
       {
         test: /\.css$/i,
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
         ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
