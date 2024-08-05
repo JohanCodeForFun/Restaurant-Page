56e5b1c9be69230add5c2abb5630bff0ca39b093
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-08-05 22:26:36 +0200

    render grill menu to DOM

--- src/index.js
+++ src/index.js
@@ -2,20 +2,33 @@ import "./styles.css";
 
 import menu from "./data/menu.json";
 
+// let menuOption = "grill"
+
 const menuContent = document.querySelector("#content");
 
-(function renderMenu() {
-  const menuTitle = document.createElement("h2");
-  menuTitle.innerText = `${Object.keys(menu)[0]}`;
-  menuContent.appendChild(menuTitle);
+(function renderMenu(option = "grill") {
+  const menuTitle = Object.keys(menu).find(element => element === option)
+  const menuTitleElement = document.createElement("h2");
+  menuTitleElement.innerText = `${menuTitle}`;
+  menuContent.appendChild(menuTitleElement);
 
   const menuList = document.createElement("ul");
-  let node, textnode;
+  let node, headerContainer, dishname, separator, dishPrice, dishDescription;
 
   for (let options of menu["grill"]) {
     node = document.createElement("li");
-    textnode = document.createTextNode(options.name)
-    node.appendChild(textnode)
+    headerContainer = document.createElement("h3");
+
+    dishname = document.createTextNode(options.name)
+    separator = document.createTextNode("------")
+    dishPrice = document.createTextNode(options.price)
+    node.appendChild(dishname)
+    node.appendChild(separator)
+    node.appendChild(dishPrice)
+
+    dishDescription = document.createTextNode(options.description);
+    node.appendChild(dishDescription)
+
     menuList.appendChild(node);
   }
 
@@ -24,11 +37,11 @@ const menuContent = document.querySelector("#content");
 
 function changeMenu() {}
 
-// const menuButtons = document.querySelectorAll(".options")
+const menuButtons = document.querySelectorAll(".options")
 
-// menuButtons.forEach(button => {
-//   button.addEventListener("click", (event) => handleMenuClick(event.target.value))
-// })
+menuButtons.forEach(button => {
+  button.addEventListener("click", (event) => handleMenuClick(event.target.value))
+})
 
 function handleMenuClick(menuOption) {
   // menuOption = value
