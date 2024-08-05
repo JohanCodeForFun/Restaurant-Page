ee6bfb320ee9eb48b4318ee4ea3530dc84c95a71
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-08-05 22:08:42 +0200

    create dom element to render grill dishnames using js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
-import './styles.css'
+import "./styles.css";
 
-import menu from './data/menu.json'
+import menu from "./data/menu.json";
 
-const menuButtons = document.querySelectorAll(".options")
+const menuContent = document.querySelector("#content");
 
-menuButtons.forEach(button => {
-  button.addEventListener("click", (event) => handleMenuClick(event.target.value))
-})
+(function renderMenu() {
+  const menuTitle = document.createElement("h2");
+  menuTitle.innerText = `${Object.keys(menu)[0]}`;
+  menuContent.appendChild(menuTitle);
 
-function handleMenuClick(value) {
-  console.log("menu button:", value)
+  const menuList = document.createElement("ul");
+  let node, textnode;
+
+  for (let options of menu["grill"]) {
+    node = document.createElement("li");
+    textnode = document.createTextNode(options.name)
+    node.appendChild(textnode)
+    menuList.appendChild(node);
+  }
+
+  menuContent.appendChild(menuList)
+})();
+
+function changeMenu() {}
+
+// const menuButtons = document.querySelectorAll(".options")
+
+// menuButtons.forEach(button => {
+//   button.addEventListener("click", (event) => handleMenuClick(event.target.value))
+// })
+
+function handleMenuClick(menuOption) {
+  // menuOption = value
+  console.log("menu option:", menuOption);
+  console.log(menu[menuOption]);
+  // menu[value].forEach(option => {
+  //   // console.log(option)
+  // })
 }
-
-console.log(menu)
